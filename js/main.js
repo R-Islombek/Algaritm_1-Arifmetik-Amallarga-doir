3325864918bc9c69db8ff563e9085faea7069f0a
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-08-22 16:44:19 +0500

    ikkinchi commentim

--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 // 1-Masala;
 
-let R = prompt("r sonini kiriting");
+// let R = prompt("r sonini kiriting");
 
-let PI = 3;
+// let PI = 3;
 
-L = 2 * PI * R,
-S = Math.pow(R, 2) * PI;
+// L = 2 * PI * R,
+// S = Math.pow(R, 2) * PI;
 
-console.log("Doiraning uzunligi =", L, "Doiraning yuzi =", S);
+// console.log("Doiraning uzunligi =", L, "Doiraning yuzi =", S);
 
+// 2-Masala
+
+// let a = prompt("a sonini kiriting");
+// let b = prompt("b sonini kiriting");
+
+// let G = Math.sqrt(a * b);
+// console.log(G);
 
