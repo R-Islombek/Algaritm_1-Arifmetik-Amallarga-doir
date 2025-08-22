886e973c9ef247a346391cb5e6756464f618690b
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-08-22 16:39:36 +0500

    birinchi masalam

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// 1-Masala;
+
+let R = prompt("r sonini kiriting");
+
+let PI = 3;
+
+L = 2 * PI * R,
+S = Math.pow(R, 2) * PI;
+
+console.log("Doiraning uzunligi =", L, "Doiraning yuzi =", S);
+
+
